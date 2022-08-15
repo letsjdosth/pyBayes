fac6ea116179c10fc0b3518ba9b5153bb8f6bd2e
Choi Seokjun <Choi.Seokjun@outlook.com>
2022-08-15 01:45:51 -0700

    DLM_kalman_tools: Kalman smoother

--- DLM_kalman_tools.py
+++ DLM_kalman_tools.py
@@ -110,7 +110,7 @@ class DLM_simulator:
         return self.theta_seq[1:], self.y_seq
 
 
-class KalmanFilter:
+class Kalman_filter_smoother:
     def __init__(self, y_observation, D0: DLM_D0_container, initial_mu0_given_D0, initial_P0_given_D0):
         self.util_inst = DLM_utility()
         
@@ -120,6 +120,7 @@ class KalmanFilter:
         self.P0 = initial_P0_given_D0
 
         #result containers
+        ## filtering
         self.theta_one_step_forecast = [] #x_t^{t-1} in Shumway-Stoffer, f_t=E(\theta_t|D_{t-1}) in West
         self.P_one_step_forecast = [] # P_t^{t-1} in Shumway-Stoffer, R_t=Var(\theta_t|D_{t-1}) in West
 
@@ -129,6 +130,11 @@ class KalmanFilter:
         self.innovation = []
         self.innovation_cov = []
         self.kalman_gain = []
+
+        ## smoothing
+        self.theta_smoothed_reversed = []
+        self.P_smoothed_reversed = []
+        self.J_smoother_gain_reversed = []
 
     #filtering
     def _filter_one_iter(self, t):
@@ -167,23 +173,50 @@ class KalmanFilter:
     def run_filter(self):
         #check everything is set
         #update
-        for t in range(self.D0.y_len):
-            self._filter_one_iter(t+1)
+        for t in range(1, self.D0.y_len+1):
+            self._filter_one_iter(t)
 
-        # delete innitial value
+        # delete initial value
         self.theta_on_time = self.theta_on_time[1:]
         self.P_on_time = self.P_on_time[1:]
 
-    def run_smoother(self, given_time=None):
-        # if given_time is None:
-        #     given_time = self.D0.y_len
-        pass #impl later
+    #smoothing
+    def _smoother_one_iter(self, t_minus_1):
+        #smoother
+        Jt1 = self.P_on_time[t_minus_1-1] @ np.transpose(self.D0.G_sys_eq_transition[t_minus_1]) @ np.linalg.inv(self.P_one_step_forecast[t_minus_1])
+        theta_t1_smoothed = self.theta_on_time[t_minus_1-1] + Jt1 @ (self.theta_smoothed_reversed[-1] - self.theta_one_step_forecast[t_minus_1])
+        Pt1_smoothed = self.P_on_time[t_minus_1-1] + Jt1 @ (self.P_smoothed_reversed[-1] - self.P_one_step_forecast[t_minus_1]) @ np.transpose(Jt1)
+
+        #save
+        self.theta_smoothed_reversed.append(theta_t1_smoothed)
+        self.P_smoothed_reversed.append(Pt1_smoothed)
+        self.J_smoother_gain_reversed.append(Jt1)
+
+    def run_smoother(self):
+        #run it after running filter
+        self.theta_smoothed_reversed.append(self.theta_on_time[-1])
+        self.P_smoothed_reversed.append(self.P_on_time[-1])
+        self.J_smoother_gain_reversed = []
+        for t_minus_1 in range(self.D0.y_len-1, 0, -1):
+            self._smoother_one_iter(t_minus_1)
+
+    # == getters ==
+    def get_filtered_on_time_theta_P(self):
+        return self.theta_on_time, self.P_on_time
+
+    def get_filtered_one_step_forecast_theta_P(self):
+        return self.theta_one_step_forecast, self.P_one_step_forecast
+
+    def get_smoothed_theta_P(self):
+        theta_smoothed = list(reversed(self.theta_smoothed_reversed))
+        P_smoothed = list(reversed(self.P_smoothed_reversed))
+        return theta_smoothed, P_smoothed
 
 
 
 if __name__=="__main__":
     import matplotlib.pyplot as plt
-    test1 = False
+    test1 = True
     test2 = True
 
     if test1:
@@ -208,15 +241,21 @@ if __name__=="__main__":
         plt.scatter(range(100), test1_y_seq, s=10) #blue dot: obs
         plt.show()
 
-        test1_filter_inst = KalmanFilter(test1_y_seq, test1_D0, np.array([0]), np.array([[1]]))
+        test1_filter_inst = Kalman_filter_smoother(test1_y_seq, test1_D0, np.array([0]), np.array([[1]]))
         test1_filter_inst.run_filter()
         test1_filtered_theta_on_time = test1_filter_inst.theta_on_time
         test1_filtered_theta_one_step_forecast = test1_filter_inst.theta_one_step_forecast
-        print(test1_filtered_theta_on_time)
+        # print(test1_filtered_theta_on_time)
         plt.plot(range(100), test1_theta_seq) #blue: true theta
         plt.plot(range(100), test1_filtered_theta_on_time) #orange: posterior E(theta_t|D_t)
         plt.plot(range(100), test1_filtered_theta_one_step_forecast) #green: prior E(theta_t|D_{t-1})
         plt.scatter(range(100), test1_y_seq, s=10) #blue dot: obs
+        
+        test1_filter_inst.run_smoother()
+        test1_smoothed_theta,_ = test1_filter_inst.get_smoothed_theta_P()
+        # print(test1_smoothed_theta)
+
+        plt.plot(range(100), test1_smoothed_theta) #red: smoothed theta
         plt.show()
 
     if test2:
@@ -244,7 +283,7 @@ if __name__=="__main__":
         # plt.plot(range(100), [x[1] for x in test2_y_seq])
         # plt.show()
 
-        test2_filter_inst = KalmanFilter(test2_y_seq, test2_D0, np.array([0,0]), np.array([[1,0],[0,1]]))
+        test2_filter_inst = Kalman_filter_smoother(test2_y_seq, test2_D0, np.array([0,0]), np.array([[1,0],[0,1]]))
         test2_filter_inst.run_filter()
         test2_filtered_theta_on_time = test2_filter_inst.theta_on_time
         test2_filtered_theta_one_step_forecast = test2_filter_inst.theta_one_step_forecast
@@ -253,4 +292,8 @@ if __name__=="__main__":
         plt.plot(range(100), [x[0] for x in test2_filtered_theta_on_time]) #orange: posterior E(theta_t|D_t)
         plt.plot(range(100), [x[0] for x in test2_filtered_theta_one_step_forecast]) #green: prior E(theta_t|D_{t-1})
         plt.scatter(range(100), [x[0] for x in test2_y_seq], s=10) #blue dot: obs
+        
+        test2_filter_inst.run_smoother()
+        test2_smoothed_theta, _ = test2_filter_inst.get_smoothed_theta_P()
+        plt.plot(range(100), [x[0] for x in test2_smoothed_theta]) #red: smoothed theta
         plt.show()
